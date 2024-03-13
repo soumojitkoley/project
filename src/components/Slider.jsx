14e2586e8fbae97f03ec5f1d174204bdd5786d90
SOUMOJIT KOLEY <koleysoumojit6@gmail.com>
2024-03-14 04:06:14 +0530

    slider performance improved

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -15,15 +15,22 @@ import './Slider.css'
 const Slider = () => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
 
+  const images = [
+    "https://images.unsplash.com/photo-1511285560929-80b456fea0bc?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1519379169146-d4b170447caa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1531747056595-07f6cbbe10ad?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1494774157365-9e04c6720e47?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  ];
+
   return (
     <div className="slider-div">
-      <Zoom delay={500} duration={2000}>
+      <Zoom delay={500} duration={3000}>
       <Swiper
         slidesPerView={isMobile ? 1 : 3}
         spaceBetween={30}
         loop={true}
         autoplay={{
-          delay: 2000,
+          delay: 1500,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -37,18 +44,15 @@ const Slider = () => {
         modules={[Autoplay, Pagination, Navigation, EffectFade, Parallax, Keyboard]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <img src="https://images.unsplash.com/photo-1511285560929-80b456fea0bc?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://images.unsplash.com/photo-1519379169146-d4b170447caa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://images.unsplash.com/photo-1531747056595-07f6cbbe10ad?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://images.unsplash.com/photo-1494774157365-9e04c6720e47?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </SwiperSlide>
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={image}
+              alt={`Slide ${index + 1}`}
+              onClick={() => openLightbox(index)}
+            />
+          </SwiperSlide>
+        ))}
       </Swiper>
       </Zoom>
     </div>
